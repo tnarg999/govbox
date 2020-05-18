b26b36cb14d7101085569bf5d26abff8f82eb15e
Grant Hugh <granthugh@Grants-MacBook-Pro-2.local>
2020-05-18 14:32:10 -0700

    t

--- policykit/policykit/dashboard.py
+++ policykit/policykit/dashboard.py
@@ -21,7 +21,7 @@ class CustomIndexDashboard(Dashboard):
         # append an app list module for "Applications"
         self.children.append(modules.AppList(
             _('Applications'),
-            exclude=('auth.*', 'CommunityActionBundle'),
+            exclude=('auth.*', 'policyengine.CommunityActionBundle'),
             column=0,
             order=0,
             deletable=False,
