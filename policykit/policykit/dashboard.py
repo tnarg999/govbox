f41c561914f23bde7a1aba081a8c71f61fe06898
Grant Hugh <granthugh@Grants-MacBook-Pro-2.local>
2020-05-18 14:21:37 -0700

    why

--- policykit/policykit/dashboard.py
+++ policykit/policykit/dashboard.py
@@ -88,7 +88,43 @@ class CustomIndexDashboard(Dashboard):
             order=2
         ))
 
-        
+    # https://github.com/geex-arts/django-jet/issues/26
+    def get_or_create_module_models(self, user):
+        module_models = []
+
+        i = 0
+
+        for module in self.children:
+            column = module.column if module.column is not None else i % self.columns
+            order = module.order if module.order is not None else int(i / self.columns)
+
+            obj, created = UserDashboardModule.objects.get_or_create(
+                title=module.title,
+                app_label=self.app_label,
+                user=user.pk,
+                module=module.fullname(),
+                column=column,
+                order=order,
+                settings=module.dump_settings(),
+                children=module.dump_children()
+            )
+            module_models.append(obj)
+            i += 1
+
+        return module_models
+
+    def load_modules(self):
+        module_models = self.get_or_create_module_models(self.context['request'].user)
+
+        loaded_modules = []
+
+        for module_model in module_models:
+            module_cls = module_model.load_module()
+            if module_cls is not None:
+                module = module_cls(model=module_model, context=self.context)
+                loaded_modules.append(module)
+
+        self.modules = loaded_modules
 
 
 
